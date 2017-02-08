a62f2db27f51d100be0a5f2a4bf9c119d7626b6a
atarutina <anatarutina@gmail.com>
2017-02-08 21:57:24 +0000

    First commit

---
+++ accounts/backends.py
@@ -0,0 +1,26 @@
+from models import User
+
+class EmailAuth(object):
+    """
+    Get and instance of User using the supplied email and check it's password
+    """
+    def authenticate(selfself, email=None, password=None):
+        try:
+            user = User.objects.get(email=email)
+            if user.check_password(password):
+                return user
+        except User.DoesNotExist:
+                return None
+
+    def get_user(self, user_id):
+        """
+        Used by the django authentication system to retrieve to retrieve an instance of User
+        """
+
+        try:
+            user = User.objects.get(pk=user_id)
+            if user.is_active:
+                return user
+            return None
+        except User.DoesNotExist:
+            return None
